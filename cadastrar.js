8c75558ef08be2d8f6a7583ba4951ad6454ea92d
laillagaleno <laillagalenoitz@gmail.com>
2024-06-25 23:42:56 -0300

    responsividade

--- cadastrar.js
+++ cadastrar.js
@@ -77,27 +77,58 @@ formularioCadastro.addEventListener("submit", function (event) {
 
   console.log(nome, email, senha, confirmarSenha);
 
-  const data = {
-    nome: nome,
-    email: email,
-    senha: senha,
+  const userData = {
+    email: "lailla@example.com",
+    senha: "senha123",
+    nome: "teste",
+    idade: 25,
+    genero: 1, // Exemplo: 0 a 3
+    estado: "SP", // Deve ser <= 2 caracteres
+    cidade: "São Paulo",
+    trilha: 2, // Exemplo: 0 a 5
+    conhece_a_cultura: 1, // Exemplo: 0 a 2
+    mais_se_interessa: 4, // Exemplo: 0 a 7
   };
 
-  alert("Cadastro realizado com sucesso!");
-  window.location.href = "../pesquisa/";
+  formData.append("email", email);
+  formData.append("senha", senha);
+  formData.append("nome", nome);
+  formData.append("idade", 25);
+  formData.append("genero", 1);
+  formData.append("estado", "SP");
+  formData.append("cidade", "São Paulo");
+  formData.append("trilha", 2);
+  formData.append("conhece_a_cultura", 1);
+  formData.append("mais_se_interessa", 4);
 
-//   fetch("https://bumbatech-api-production.up.railway.app/users/create-user", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(data),
-//   })
-//     .then((data) => {
-//       console.log(data);
-//       alert("Cadastro realizado com sucesso!");
-//       window.location.href = "../pesquisa/";
-//     })
-//     .catch((error) => console.error("Erro:", error));
+console.log(JSON.stringify(userData))
+  cadastrarUsuario(formData);
+});
+
+async function cadastrarUsuario(userData) {
+  const url =
+    "https://bumbatech-api-production.up.railway.app/users/create-user";
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      mode: 'no-cors',
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "multipart/form-data'",
+      },
+      body: userData,
+    });
+
+    if (!response.ok) {
+      throw new Error(`Erro na solicitação: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log("Usuário criado com sucesso:", data);
+    alert("Cadastro realizado com sucesso!");
+    window.location.href = "../pesquisa/";
+  } catch (error) {
+    console.error("Erro ao criar usuário:", error);
+  }
 }
-);
